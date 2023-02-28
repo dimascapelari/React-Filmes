07b03e062ad3f48737892b5c0b878a4e2d8a5df3
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-02-28 16:44:29 -0300

    React Filmes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  //   console.log(moviesURL, apiKey);
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    console.log("topRatedUrl", topRatedUrl);
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  //   console.log("topMovies", topMovies);
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        {/* {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)} */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
